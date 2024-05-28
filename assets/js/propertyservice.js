d78d15f02c026fb5e5572a7a1a1ac5c37fb86a00
Rohit Mallikarjun <devoper.rohit@gmail.com>
2024-05-28 20:21:44 +0530

    12.2C- bug fix

--- assets/js/propertyservice.js
+++ assets/js/propertyservice.js
@@ -102,7 +102,7 @@ function displayAllRentProperty(properties){
             <div class="col-lg-4 col-md-6 fadeInUp">
                 <div class="property-item rounded overflow-hidden">
                     <div class="position-relative overflow-hidden">
-                        <a><img class="img-fluid" src="assets/img/property-1.jpg" alt=""></a>
+                        <a><img class="img-fluid" src="assets/img/property-1.jpg" alt="property img"></a>
                         <div class="bg-primary rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">For ${property.Availability}</div>
                         <div class="bg-white rounded-top text-primary position-absolute start-0 bottom-0 mx-4 pt-1 px-3">${property.property_type}</div>
                     </div>
